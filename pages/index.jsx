7412d9693e27f5a08931a25a9539fcb771badd2e
Fahmi Fachrizal <mff.online.acc@gmail.com>
2023-08-25 10:10:19 +0700

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -81,7 +81,7 @@ function OverviewPage({ session, following }) {
                   <div className='h-full w-full flex flex-col'>
                     <h3 className='text-lg font-semibold pb-2'>{followNASA?`That's Great You Are Following NASA`:`Why are you not following NASA? Are you...`}</h3>
                     <div className='flex-1 relative overflow-clip rounded-xl'>
-                      <img src="https://pbs.twimg.com/profile_banners/11348282/1679344008/1500x500" alt="NASA" className='object-none h-full w-full rounded-xl group-hover:scale-150 duration-500 group-hover:rotate-12'/>
+                      <img src="https://media.wired.com/photos/59268c5dcfe0d93c474309a2/master/w_1600,c_limit/BSP_054.jpg" alt="NASA" className='object-none h-full w-full rounded-xl group-hover:scale-150 duration-500 group-hover:rotate-12'/>
                       <div className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 flex flex-col items-center justify-center gap-y-2'>
                         <img src="https://pbs.twimg.com/profile_images/1321163587679784960/0ZxKlEKB_400x400.jpg" alt="NASA" className=' w-16 h-16 bg-white rounded-lg p-1'/>
                         <button className={`text-xs text-gray-800 bg-white ${followNASA?'':'hover:bg-[#03a9f4] hover:text-white'}  font-medium rounded-full px-2 py-1`} disabled={followNASA}
